cf79101495f806b4d75e87fff8040e9408e38fda
shambhavir <shambhavi.ramaswamy@gmail.com>
2021-03-23 20:04:06 -0400

    dashboard

---
+++ src/screens/DashBoard/DashBoard.js
@@ -0,0 +1,70 @@
+import React, { useState, Component } from 'react'
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import styles from './styles';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import { firebase, firebaseConfig, db, getUserDocument, realtime } from '../../firebase/config'
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/database";
+const user = firebase.auth().currentUser;
+var data1 = [];
+
+
+export default class MoreInfo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state =
+        {
+            bp: '',
+            ag: '',
+            mp: '',
+            intVal: [],
+            dash: ''
+        };
+    }
+    componentDidMount() {
+        //console.log(data1);
+        const keys = []
+
+        const userRef = firebase.database().ref("users");
+        const uidRef = firebase.database().ref("users/" + firebase.auth().currentUser.uid);
+        const bpRef = firebase.database().ref("users/" + firebase.auth().currentUser.uid + "/" + "BloodPressure")
+        const path = bpRef.toString();
+
+        bpRef.push({
+            bpMeasure: this.state.bp,
+            date: Date.now(),
+        })
+
+
+        bpRef.once('value').then(snapshot => {
+            snapshot.forEach(item => {
+                var temp = { bp1: item.val().bpMeasure };
+               
+                data1.push(Object.values(temp));
+                this.setState({ data1 })
+                //this.dash = temp; 
+                this.setState({ dash: Object.values(temp) });
+
+                this.setState({ intVal: Object.values(temp) })
+                
+                return false;
+            });
+           
+        });
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                    <View style={styles.innerContainer}>
+                {data1.map((d, i) => (
+                    <Text key={i}>{d}</Text>
+                ))}
+            </View>
+            </View>
+
+        )
+    }
+}
