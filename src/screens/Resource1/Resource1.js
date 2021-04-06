19b253a2a22ca4a158d7868fff77d91c536ca47b
shambhavir <shambhavi.ramaswamy@gmail.com>
2021-04-06 09:14:12 -0400

    resource screen

--- src/screens/Resource1/Resource1.js
+++ src/screens/Resource1/Resource1.js
@@ -8,7 +8,7 @@ import { Card } from 'react-native-shadow-cards';
 
 const user = firebase.auth().currentUser;
 import styles from './styles';
-var infostorage = []; 
+var infostorage = [];
 infostorage[0] = "this is some data 1"
 infostorage[1] = "this is some data 2"
 infostorage[2] = "this is some data 3"
@@ -72,61 +72,61 @@ export default class Resource1 extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                 <ScrollView>
-                <Card style={{ padding: 10, margin: 10, borderRadius: 20, height: 300 }}>
-               
-                    <Text>Title</Text>
-                    <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit</Text>
-                    
+                <ScrollView>
+                    <Card style={{ padding: 10, margin: 10, borderRadius: 20, height: 300 }}>
 
-                    <Button
-                        onPress={this.updateInfo1}
-                        title="❤️"
-                        color="#841584"
-                        accessibilityLabel="Learn more about this purple button"
-                    />
-                   
-                </Card>
-                <Card style={{ padding: 10, margin: 10, borderRadius: 20, height: 300  }}>
-                    <Text>Title 2</Text>
-                    <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit</Text>
-                    <Button
-                        onPress={this.updateInfo2}
-                        title="❤️"
-                        color="#841584"
-                        accessibilityLabel="Learn more about this purple button"
-                    />
-                </Card>
-                <Card style={{ padding: 10, margin: 10, borderRadius: 20, height: 300  }}>
-                    <Text>info part3</Text>
-                    <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit</Text>
-                    <Button
-                        onPress={this.updateInfo3}
-                        title="❤️"
-                        color="#841584"
-                        accessibilityLabel="Learn more about this purple button"
-                    />
-                </Card>
-                <Card style={{ padding: 10, margin: 10, borderRadius: 20, height: 300 }}>
-                    <Text>info part4</Text>
-                    <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit</Text>
-                    <Button
-                        onPress={this.updateInfo4}
-                        title="❤️"
-                        color="#841584"
-                        accessibilityLabel="Learn more about this purple button"
-                    />
-                </Card>
-                <Card style={{ padding: 10, margin: 10, borderRadius: 20, height: 300 }}>
-                    <Text>info part4</Text>
-                    <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit</Text>
-                    <Button
-                        onPress={this.updateInfo5}
-                        title="❤️"
-                        color="#841584"
-                        accessibilityLabel="Learn more about this purple button"
-                    />
-                </Card>
+                        <Text>Title</Text>
+                        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit</Text>
+
+
+                        <Button
+                            onPress={this.updateInfo1}
+                            title="❤️"
+                            color="#841584"
+                            accessibilityLabel="Learn more about this purple button"
+                        />
+
+                    </Card>
+                    <Card style={{ padding: 10, margin: 10, borderRadius: 20, height: 300 }}>
+                        <Text>Title 2</Text>
+                        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit</Text>
+                        <Button
+                            onPress={this.updateInfo2}
+                            title="❤️"
+                            color="#841584"
+                            accessibilityLabel="Learn more about this purple button"
+                        />
+                    </Card>
+                    <Card style={{ padding: 10, margin: 10, borderRadius: 20, height: 300 }}>
+                        <Text>info part3</Text>
+                        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit</Text>
+                        <Button
+                            onPress={this.updateInfo3}
+                            title="❤️"
+                            color="#841584"
+                            accessibilityLabel="Learn more about this purple button"
+                        />
+                    </Card>
+                    <Card style={{ padding: 10, margin: 10, borderRadius: 20, height: 300 }}>
+                        <Text>info part4</Text>
+                        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit</Text>
+                        <Button
+                            onPress={this.updateInfo4}
+                            title="❤️"
+                            color="#841584"
+                            accessibilityLabel="Learn more about this purple button"
+                        />
+                    </Card>
+                    <Card style={{ padding: 10, margin: 10, borderRadius: 20, height: 300 }}>
+                        <Text>info part4</Text>
+                        <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit</Text>
+                        <Button
+                            onPress={this.updateInfo5}
+                            title="❤️"
+                            color="#841584"
+                            accessibilityLabel="Learn more about this purple button"
+                        />
+                    </Card>
                 </ScrollView>
             </View>
         )
